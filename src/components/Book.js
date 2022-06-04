9697340542093af15dae485bdad63e83e89bead9
bamsi <hibamsi@gmail.com>
2022-06-04 10:31:07 +0300

    update style font family

--- src/components/Book.js
+++ src/components/Book.js
@@ -6,9 +6,9 @@ const Book = () => (
     <h2>The Hunger Games</h2>
     <span>Suzanne Collins</span>
     <ul className="action">
-      <li>Comments</li>
-      <li>Remove</li>
-      <li>Edit</li>
+      <li><button type="button">Comments</button></li>
+      <li><button type="button">Remove</button></li>
+      <li><button type="button">Edit</button></li>
     </ul>
   </div>
 );
