d80f9c4438a858c18e7582a85521e7458b3be018
bamsi <hibamsi@gmail.com>
2022-06-11 17:18:16 +0300

    add style to add new book form

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -2,6 +2,7 @@ import { React, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { useDispatch } from 'react-redux';
 import { saveBook } from '../redux/books/books';
+import './AddBook.css';
 
 const AddBook = () => {
   const [title, setTitle] = useState('');
@@ -33,27 +34,31 @@ const AddBook = () => {
   };
 
   return (
-    <section>
-      <h2>Add New Book</h2>
+    <section className="add-book-section">
+      <h2 className="gray-color add-book-title">Add New Book</h2>
       <form onSubmit={(e) => handleSubmit(e)}>
-        <input
-          type="text"
-          name="title"
-          value={title}
-          onChange={(e) => changeTitle(e)}
-          placeholder="Book title"
-        />
-        <input
-          type="text"
-          name="author"
-          value={author}
-          onChange={(e) => changeAuthor(e)}
-          placeholder="Author"
-        />
-        <select name="category">
-          <option value="">category</option>
-        </select>
-        <button type="submit">Add book</button>
+        <div className="book-form">
+          <input
+            type="text"
+            name="title"
+            value={title}
+            className="input-text"
+            onChange={(e) => changeTitle(e)}
+            placeholder="Book title"
+          />
+          <input
+            type="text"
+            name="author"
+            value={author}
+            className="input-text"
+            onChange={(e) => changeAuthor(e)}
+            placeholder="Author"
+          />
+          <select name="category" className="input-text">
+            <option value="">Category</option>
+          </select>
+          <button type="submit" className="submit-button">Add book</button>
+        </div>
       </form>
     </section>
   );
